66e342fa56e9b299fdeec85bcc0b0ed2e056f6f4
unknown <nate15.david.edberg@nykopingsgymnasium.com>
2017-11-11 10:05:20 +0100

    push

--- build/java/navbar.js
+++
@@ -1,8 +0,0 @@
-function toggleDropDown() {
-    var x = document.getElementById("dropdown-content").style.maxWidth = "700px";
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
